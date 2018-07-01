3f35fb3d208d71d32d6ff06196326e74a541208e
rag594 <raghavrastogi594@gmail.com>
2018-07-01 22:51:36 +0530

    change keyword to url

--- src/main/java/com/ep/LinkExtractor/ExtractionInterface/LinkFunc.java
+++ src/main/java/com/ep/LinkExtractor/ExtractionInterface/LinkFunc.java
@@ -3,7 +3,7 @@ package com.ep.LinkExtractor.ExtractionInterface;
 import java.util.ArrayList;
 
 public interface LinkFunc {
-    public String getLinksFromReddit(String keyword);
+    public String getLinksFromReddit(String url);
     public String getContent(String url);
     public String getHTML(String url);
     public ArrayList<String> getSubreddits(String url);
